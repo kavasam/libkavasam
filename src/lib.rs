7fba0503e56f1a6de02f364bfa70a201e9c70f83
realaravinth <realaravinth@batsense.net>
2021-12-19 20:47:50 +0530

    ASCII armored representation of SignedHash

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@
  */
 use multihash::derive::Multihash;
 use multihash::typenum::{U32, U64};
+use base64::{encode, decode};
 use multihash::{
     Blake2b256, Blake2bDigest, MultihashDigest, MultihashGeneric, Sha2Digest, Sha2_256,
     Sha3Digest, Sha3_256,
@@ -24,18 +25,22 @@ use multihash::{
 use serde::{Deserialize, Serialize};
 
 pub mod id;
+pub mod errors;
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
+use id::Identity;
+use errors::ServiceResult;
+
+#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
 /// Represents a signed hash message, the hash being the digest of
 /// the identifier(phone number,email address, etc.)
 pub struct SignedHash {
-    hash: MultihashGeneric<U64>,
-    sign: Vec<u8>,
+    pub hash: MultihashGeneric<U64>,
+    pub sign: Vec<u8>,
 }
 
 impl SignedHash {
     /// Generate new signed message
-    pub fn new(hash: MultihashGeneric<U64>, id: &id::Identity) -> Self {
+    pub fn new(hash: MultihashGeneric<U64>, id: &Identity) -> Self {
         let sign = id.sign(&hash.to_bytes()).as_ref().to_owned();
         Self { hash, sign }
     }
@@ -43,6 +48,45 @@ impl SignedHash {
     /// Verify a signed message
     pub fn verify(&self, public_key: &id::PublicKey) -> bool {
         public_key.verify(&self.hash.to_bytes(), &self.sign)
+    }
+
+    /// Get ASCII armored representation of [Self]
+    pub fn ascii_armor(&self) -> SignedHashAsciiArmored {
+        SignedHashAsciiArmored {
+        sign: encode(&self.sign),
+            hash: encode(&self.hash.to_bytes()),
+        }
+    }
+
+    /// String representation
+    pub fn from_ascii_armor(ascii_armor: &SignedHashAsciiArmored) -> ServiceResult<Self> {
+        ascii_armor.to_signed_hash()
+    }
+
+}
+
+#[derive(Deserialize, PartialEq, Serialize, Debug, Clone)]
+/// ASCII armored representation of [SignedHash]
+pub struct SignedHashAsciiArmored {
+    /// Hash in ASCII
+    pub hash: String,
+    /// Signature in ASCII
+    pub sign: String
+}
+
+impl SignedHashAsciiArmored {
+    /// Create new [Self] from [SignedHash]
+    pub fn new(signed_hash: &SignedHash) -> Self {
+        signed_hash.ascii_armor()
+    }
+
+    /// Get [SignedHash]
+    pub fn to_signed_hash(&self) -> ServiceResult<SignedHash> {
+        Ok( SignedHash{
+            hash: MultihashGeneric::from_bytes(&decode(&self.hash)?)?,
+            sign: decode(&self.sign)?,
+        })
+
     }
 }
 
@@ -83,7 +127,7 @@ pub struct ReportMessage {
 
 impl ReportMessage {
     /// Generate a new message to report an identifier
-    pub fn new(data: &[u8], id_type: IDType, id: &id::Identity) -> Self {
+    pub fn new(data: &[u8], id_type: IDType, id: &Identity) -> Self {
         let hashes = [
             SignedHash::new(Code::Sha2_256.digest(data), id),
             SignedHash::new(Code::Sha3_256.digest(data), id),
@@ -115,11 +159,12 @@ mod tests {
     use super::*;
     use multihash::Code as MCode;
 
+    const DATA: &[u8] = b"fooabr";
+
     #[test]
     fn signed_hash_work() {
-        let (_pkcs8_bytes, id) = id::Identity::new();
+        let (_pkcs8_bytes, id) = Identity::new();
         let public_key = id.pub_key();
-        const DATA: &[u8] = b"fooabr";
         let hash1 = Code::Sha3_256.digest(DATA);
         let hash2 = Code::Sha3_256.digest(b"barfoo");
         let mut signed_hash = SignedHash::new(hash1, &id);
@@ -127,17 +172,36 @@ mod tests {
             signed_hash.verify(&public_key),
             "signature successfully verified"
         );
+
         signed_hash.hash = hash2;
         assert!(
             !signed_hash.verify(&public_key),
             "signature verification failed, invalid signature"
         );
+
+    }
+
+    #[test]
+    fn ascii_armor_works() {
+        let (_pkcs8_bytes, id) = Identity::new();
+        let public_key = id.pub_key();
+        let hash1 = Code::Sha3_256.digest(DATA);
+        let signed_hash = SignedHash::new(hash1, &id);
+
+        let ascii_armored = signed_hash.ascii_armor();
+        assert_eq!(ascii_armored, SignedHashAsciiArmored::new(&signed_hash));
+
+
+        let from_ascii_armor = ascii_armored.to_signed_hash().unwrap();
+        assert_eq!(SignedHash::from_ascii_armor(&ascii_armored).unwrap(), from_ascii_armor);
+
+        assert_eq!(from_ascii_armor, signed_hash);
+        assert!(from_ascii_armor.verify(&public_key))
     }
 
     #[test]
     fn report_messages_work() {
-        let (_pkcs8_bytes, id) = id::Identity::new();
-        const DATA: &[u8] = b"fooabr";
+        let (_pkcs8_bytes, id) = Identity::new();
         let mut msg = ReportMessage::new(DATA, IDType::PhoneNumber, &id);
         assert!(msg.verify());
 
@@ -151,9 +215,8 @@ mod tests {
 
     #[test]
     fn custom_table_matches_multihash_impl() {
-        let x = b"1234123455";
-        assert_eq!(Code::Sha3_256.digest(x), MCode::Sha3_256.digest(x));
-        assert_eq!(Code::Sha2_256.digest(x), MCode::Sha2_256.digest(x));
-        assert_eq!(Code::Blake2b256.digest(x), MCode::Blake2b256.digest(x));
+        assert_eq!(Code::Sha3_256.digest(DATA), MCode::Sha3_256.digest(DATA));
+        assert_eq!(Code::Sha2_256.digest(DATA), MCode::Sha2_256.digest(DATA));
+        assert_eq!(Code::Blake2b256.digest(DATA), MCode::Blake2b256.digest(DATA));
     }
 }
